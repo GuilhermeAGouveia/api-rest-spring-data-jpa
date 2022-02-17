19dc95e2242ffbbdb9de3d69582d173f370f267f
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2022-02-17 20:14:37 -0300

    Refactoring in Student Controller

--- src/main/java/com/learn/sdj/Repository/StudentRepository.java
+++ src/main/java/com/learn/sdj/Repository/StudentRepository.java
@@ -4,8 +4,6 @@ import com.learn.sdj.Entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query("select student.firstName from Student student where student.id = ?1 ")
     String findFirstNameById(Long id);
